4a971a546d312a61a804e1c3564ccb28cd1de95c
gydnjs <khw33352182@gmail.com>
2024-09-23 20:59:58 +0900

    .

---
+++ @Resources/Script/Controller/GloveController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveController : BaseController
+{
+    protected override void Init()
+    {
+        base.Init();
+    }
+}
